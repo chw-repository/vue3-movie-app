ff77a4329f120ee65c1b1450b361d37f1a5d0ad3
chw-repository <chw.repository@gmail.com>
2023-03-05 11:45:10 +0900

    Search

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <Headline />
+  <Search />
 </template>
 
 <script>
 import Headline from "../components/Headline.vue";
+import Search from "../components/Search.vue";
 
 export default {
   name: "Home",
-  components: {Headline}
+  components: {Search, Headline}
 }
 </script>
 
